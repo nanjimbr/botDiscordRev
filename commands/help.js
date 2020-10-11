e36ed3bb596f13657a7a00425ca54568df90adeb
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-12 00:48:01 +0200

    fix embed

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
         {name: "__!help__", value: "Affiche ce message d'aide."},
         {name: "__!fart__", value: "Ne fonctionne que quand tu es dans un channel vocal, fais faire un pet au bot."},
         {name: "__!avatar__ *@utilisateur*", value: "Ne fonctionne que quand tu es dans un channel vocal, fais faire un pet au bot."},
-        {name: "__!img__ *mot-clé de recherche*", value: "Affiche une image en rapport avec les mots-clés de ta recherche."},
+        {name: "__!img__ *mot-clé de recherche*", value: "Affiche ta photo de profil ou celle de l'*utilisateur* mentionné."},
         {name: "__!say__ *message*", value: "Fais dire un message anonymement, par le bot."}
     )
     message.channel.send(exampleEmbed);

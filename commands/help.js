920f0a611d239a77c79368d2d70d9b566565906e
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-12 00:59:53 +0200

    improved help command

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
         {name: "__!avatar__ *@utilisateur*", value: "Ne fonctionne que quand tu es dans un channel vocal, fais faire un pet au bot."},
         {name: "__!img__ *mot-clé de recherche*", value: "Affiche ta photo de profil ou celle de l'*utilisateur* mentionné."},
         {name: "__!say__ *message*", value: "Fais dire un message anonymement, par le bot."},
-        {name: "__!del__ *nombre < 100", value: "Supprime le nombre de messages spécifié. Ne peut pas supprimer les messages plus vieux que 14 jours, pas plus de 99 messages."},
+        {name: "__!del__ *nombre < 100*", value: "Supprime le nombre de messages spécifié. Ne peut pas supprimer les messages plus vieux que 14 jours, pas plus de 99 messages."},
     )
     message.channel.send(exampleEmbed);
     console.log(message.author.tag + " used !help");

cad5798b4dbdbab88280b6932d39214239705c52
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-13 22:11:41 +0200

    add allo command w/ mp3

---
+++ commands/allo.js
@@ -0,0 +1,12 @@
+exports.run = async(client, message, args) => {
+    var allo = './sounds/startup-palmashow_Trim.mp3';
+
+    if(message.member.voice.channel) {
+        message.member.voice.channel.join().then(connection => { connection.play(ludo, { volume: 1})});
+            message.delete();
+            console.log(message.author.tag + " used !allo");
+    } else {
+        message.reply('Tu n\'es pas dans un channel vocal :/');
+        message.delete();
+    }
+}

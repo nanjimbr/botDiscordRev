26268197f131922d2e5197f0bf20bb6a881fba40
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-13 22:14:34 +0200

    fix allo

--- commands/allo.js
+++ commands/allo.js
@@ -2,7 +2,7 @@ exports.run = async(client, message, args) => {
     var allo = './sounds/startup-palmashow_Trim.mp3';
 
     if(message.member.voice.channel) {
-        message.member.voice.channel.join().then(connection => { connection.play(ludo, { volume: 1})});
+        message.member.voice.channel.join().then(connection => { connection.play(allo, { volume: 1})});
             message.delete();
             console.log(message.author.tag + " used !allo");
     } else {

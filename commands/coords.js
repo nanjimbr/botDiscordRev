9c9309f1f5e14046bc1b22060ecc29074df45e95
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-14 19:00:40 +0200

    fix coords

--- commands/coords.js
+++ commands/coords.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args) => {
-    message.channel.send('Les coordonnées du spawn (Overworld) sont : -45, 200')
-    message.channel.send('Les coordonnées du spawn (Nether) sont : -13, 110, 10')
+    message.channel.send('Les coordonnées du spawn (__Overworld__) sont : *-45, 200*')
+    message.channel.send('Les coordonnées du spawn (__Nether__) sont : *-13, 110, 10*')
+    message.delete();
 }

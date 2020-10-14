9638b2f3ab936a636e957ad937fc0d34cabd86f0
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-14 18:58:57 +0200

    add coords minecraft

---
+++ commands/coords.js
@@ -0,0 +1,4 @@
+exports.run = (client, message, args) => {
+    message.channel.send('Les coordonnées du spawn (Overworld) sont : -45, 200')
+    message.channel.send('Les coordonnées du spawn (Nether) sont : -13, 110, 10')
+}

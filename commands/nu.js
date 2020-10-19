4a86435732c1e8917ed562a2c21143942c103cdc
nanjimbr <nanji.mendiboure@gmail.com>
2020-10-19 23:18:55 +0200

    add multiple commands and sounds

--- commands/nu.js
+++ commands/nu.js
@@ -1,10 +1,10 @@
 exports.run = async(client, message, args) => {
-    var enorme = './sounds/oui-lola.mp3';
+    var nu = './sounds/nu.mp3';
 
     if(message.member.voice.channel) {
-        message.member.voice.channel.join().then(connection => { connection.play(enorme, { volume: 1})});
+        message.member.voice.channel.join().then(connection => { connection.play(nu, { volume: 1})});
             message.delete();
-            console.log(message.author.tag + " used !enorme");
+            console.log(message.author.tag + " used !nu");
     } else {
         message.reply('Tu n\'es pas dans un channel vocal :/');
         message.delete();
--- commands/power.js
+++ commands/power.js
@@ -1,10 +1,10 @@
 exports.run = async(client, message, args) => {
-    var enorme = './sounds/oui-lola.mp3';
+    var power = './sounds/power.mp3';
 
     if(message.member.voice.channel) {
-        message.member.voice.channel.join().then(connection => { connection.play(enorme, { volume: 1})});
+        message.member.voice.channel.join().then(connection => { connection.play(power, { volume: 1})});
             message.delete();
-            console.log(message.author.tag + " used !enorme");
+            console.log(message.author.tag + " used !power");
     } else {
         message.reply('Tu n\'es pas dans un channel vocal :/');
         message.delete();

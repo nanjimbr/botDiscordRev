5b4fefb3c6aedb227810ebb3b1f4c2b97e742b00
unknown <nanji.mendiboure@gmail.com>
2020-10-22 22:13:39 +0200

    added cri

---
+++ commands/cri.js
@@ -0,0 +1,12 @@
+exports.run = async(client, message, args) => {
+    var cri = './sounds/Le_cri.mp3';
+
+    if(message.member.voice.channel) {
+        message.member.voice.channel.join().then(connection => { connection.play(cru, { volume: 1})});
+            message.delete();
+            console.log(message.author.tag + " used !cri");
+    } else {
+        message.reply('Tu n\'es pas dans un channel vocal :/');
+        message.delete();
+    }
+}

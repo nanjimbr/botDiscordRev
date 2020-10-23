68aa2b1f08b1a9546ad7842310bdc3aba9202b89
unknown <nanji.mendiboure@gmail.com>
2020-10-23 19:01:00 +0200

    added rps

--- commands/ppp.js
+++ commands/ppp.js
@@ -4,6 +4,6 @@ function random() { return `${rps[Math.floor(Math.random() * Math.floor(2))]}!` 
 exports.run = (client, msg, args) => {
     let choice = args.join(" ").toLowerCase();
     if (choice === '') return msg.reply("Dites \"pierre\", \"papier\" ou \"ciseaux\".");
-    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") return msg.reply(`${choice} n'est pas un des choix :)`);
+    if (choice !== "pierre" && choice !== "papier" && choice !== "ciseaux") return msg.reply(`${choice} n'est pas un des choix :)`);
     msg.reply(random());
 }

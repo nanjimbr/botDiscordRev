1d5ef764797cb2003b9aeaffd32c39785fcb43b6
unknown <nanji.mendiboure@gmail.com>
2020-10-23 03:23:09 +0200

    modified help

--- commands/help.js
+++ commands/help.js
@@ -7,11 +7,8 @@ exports.run = (client, message, args) => {
     .setColor('#47ff00')
     .addFields(
         {name: "__!help__", value: "Affiche ce message d'aide."},
-        {name: "__!fart__", value: "Ne fonctionne que quand tu es dans un channel vocal, fais faire un pet au bot."},
-        {name: "__!avatar__ *@utilisateur*", value: "Affiche ta photo de profil ou celle de l'*utilisateur* mentionné."},
-        {name: "__!img__ *mot-clé de recherche*", value: "Recherche une image correspondant aux *mots-clés* utilisés."},
-        {name: "__!say__ *message*", value: "Fais dire un message anonymement, par le bot."},
-        {name: "__!del__ *nombre < 100*", value: "Supprime le nombre de messages spécifié. Ne peut pas supprimer les messages plus vieux que 14 jours, pas plus de 99 messages."},
+        {name: "__Sons__", value: "**!aladdin**, ** !allo**, **!anus**, **!aziz**, **!bite**, **!caca**, **!cesoir**, **!cri**, **!doigt**, **!emergency**, **!enorme**, **!fart**, **!fion**, **!golmont**, **!gueule**, **!letsgo**, **!ludo**, **!nu**, **!power**, **!queue**, **!rouge**"},
+        {name: "__General__", value: "**!avatar**, **!del**, **!img**, **!say**"}
     )
     message.channel.send(exampleEmbed);
     console.log(message.author.tag + " used !help");

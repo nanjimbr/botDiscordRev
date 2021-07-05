3b378d9aac8fa39fad0f9bbbdcd4d983fe491f9a
unknown <nanji.mendiboure@gmail.com>
2021-07-05 15:43:27 +0200

    music

--- commands/tracks.js
+++ commands/tracks.js
@@ -1,7 +1,13 @@
 exports.run = (client, message, args) => {
     let queue = client.player.getQueue(message);
-    message.channel.send('Voici les prochains titres :\n' + queue.songs.map((song, id) =>
+    if(!queue.songs){
+        message.channel.send("y'a po de titres dans la liste là");
+        message.delete();
+    } else {
+        message.channel.send('Voici les prochains titres :\n' + queue.songs.map((song, id) =>
         `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
     ).slice(0, 10).join("\n"));
     message.delete();
 }
+    }
+    
